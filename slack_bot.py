ba55ca1c3fb1614306ae82c12873f768423fa061
RaazKH <raazkhoshnood@gmail.com>
2023-06-28 21:11:52 +0300

    feat: added set-live command

--- slack_bot.py
+++ slack_bot.py
@@ -23,9 +23,10 @@ client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
 # TESTING - delete later
-client.chat_postMessage(channel="#testc", text="ready to go boss")
+client.chat_postMessage(channel="#testc", text=":robot_face: ready to go boss :robot_face:")
 
 # Increasing this will slow down response time
+# make this an env variable?
 maxDeploys = 5
 
 def list_site_deploys():
@@ -73,6 +74,18 @@ def rollback_netlify_site():
         print("Site unlock error!")
         return "roll-error"
 
+def set_netlify_live_deploy(deploy_id):
+    headers = {'Authorization': f"Bearer {API_TOKEN}"}
+    url = f"https://api.netlify.com/api/v1/sites/{SITE_NAME}/deploys/{deploy_id}/restore"
+
+    response = requests.post(url, headers=headers)
+    if response.status_code == 200:
+        print("Restored!")
+        return "restored"
+    else:
+        print("Restore site deploy error!")
+        return "restore-error"
+
 @app.route('/list-deploys', methods=['GET', 'POST'])
 def listDeploys():
     data = request.form
@@ -94,13 +107,13 @@ def listDeploys():
         return Response(), 200
 
     response_json = json.loads(siteList)
-    message = f":robot_face: *List of {numOfDeploys} most recent deploys to Netlify:* :robot_face:\n\n---\n\n"
+    message = f":robot_face: *List of {numOfDeploys} most recent deploys to Netlify* :robot_face:\n\n---\n\n"
     for entry in response_json[:numOfDeploys]:
         time_stamp = entry['created_at']
         deploy_id = entry['id']
         status = entry['state']
         deploy_url = entry['deploy_url']
-        message += f"Created: {time_stamp}\nBuild ID: {deploy_id}\nStatus: {status}\nDeploy Preview Link: {deploy_url}\n\n---\n\n"
+        message += f"Created: {time_stamp}\nBuild ID: {deploy_id}\nStatus: `{status}`\nDeploy Preview Link: {deploy_url}\n\n---\n\n"
     client.chat_postMessage(channel=channel_id, text=message)
     return Response(), 200
 
@@ -170,6 +183,53 @@ def rollback():
         client.chat_postMessage(channel=channel_id, text=":robot_face: Site rolled back! :robot_face:")
     else:
         client.chat_postMessage(channel=channel_id, text=":robot_face: Not rolled back - ERROR! :robot_face:")
+    return Response(), 200
+
+@app.route('/set-live', methods=['POST'])
+def setLive():
+    data = request.form
+    channel_id = data.get('channel_id')
+    deploy_id = data.get('text')
+
+    if deploy_id != "":
+        if len(deploy_id) != 24:
+            client.chat_postMessage(channel=channel_id, text=":robot_face: Please enter valid deploy ID or don't follow the command with any text; you can see the most recent deploys using the list-deploys command! :robot_face:")
+            return Response(), 200
+
+    siteList = list_site_deploys()
+    if siteList == "error":
+        client.chat_postMessage(channel=channel_id, text=":robot_face: Error in list_site_deploys response! :robot_face:")
+        return Response(), 200
+
+    response_json = json.loads(siteList)
+
+    # case 1: no deploy ID was passed
+    if deploy_id == "":
+        for entry in response_json[:maxDeploys]:
+            if entry['state'] == 'ready':
+                message = set_netlify_live_deploy(entry['id'])
+                if message == "restored":
+                    client.chat_postMessage(channel=channel_id, text=f":robot_face: Site live deploy set to: {deploy_id}! :robot_face:")
+                else:
+                    client.chat_postMessage(channel=channel_id, text=":robot_face: Deploy not updated - ERROR! :robot_face:")
+                return Response(), 200
+        client.chat_postMessage(channel=channel_id, text=f":robot_face: No deploys found in {maxDeploys} most recent deploys which are eligable for publishing, please contact one of the Devs! :robot_face:")
+        return Response(), 200
+
+    # case 2: deploy ID was passed
+    for entry in response_json[:maxDeploys]:
+        if deploy_id == entry['id']:
+            if entry['state'] != 'ready':
+                client.chat_postMessage(channel=channel_id, text=f":robot_face: Deploy requested currently has the `{entry['state']}` status, deploy must have `ready` status! :robot_face:")
+                return Response(), 200
+
+            message = set_netlify_live_deploy(entry['id'])
+            if message == "restored":
+                client.chat_postMessage(channel=channel_id, text=f":robot_face: Site live deploy set to: {deploy_id}! :robot_face:")
+            else:
+                client.chat_postMessage(channel=channel_id, text=":robot_face: Deploy not updated - ERROR! :robot_face:")
+            return Response(), 200
+    client.chat_postMessage(channel=channel_id, text=f":robot_face: Deploy not found, please choose from one of the {maxDeploys} most recent deploys! :robot_face:")
     return Response(), 200
 
 if __name__ == "__main__":
